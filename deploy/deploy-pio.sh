4b93baaa92f2b5222f6b8c8f29c45114ec43aa05
Jose Ibanez <ibanez.j@gmail.com>
2019-05-24 17:53:32 +0200

    lib update

--- deploy/deploy-pio.sh
+++ deploy/deploy-pio.sh
@@ -11,5 +11,5 @@ pip install -U platformio
 #udev rules
 curl -fsSL https://raw.githubusercontent.com/platformio/platformio-core/develop/scripts/99-platformio-udev.rules | sudo tee /etc/udev/rules.d/99-platformio-udev.rules
 
-
-
+platformio update
+platformio lib update
